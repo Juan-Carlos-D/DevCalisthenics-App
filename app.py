933e1b3ecf6249eaf6c97563ea4c6061adc99d52
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-08-20 09:56:48 -0700

    app py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+# from faq import faq_data 
+# from macronutrients import macronutrients_data
+# from macroscalc import calculate_macros
+# from exercises import exercises
+# from blogs import blogs
+import re
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
