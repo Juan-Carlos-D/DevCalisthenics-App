3f753b2e63185843058110b841cacf689e25d09c
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-08-21 11:02:21 -0700

    programs

--- programs.py
+++ programs.py
@@ -81,5 +81,43 @@ programs_data = {
             "../../static/assets/Programs/Base/Programs/streetworkout.webp"
         ]
     },
-    # Add other programs here
+    "muscle_up_program": {
+        "title": "Welcome to the Muscle-Up Program!",
+        "intro_image": "../../static/assets/Programs/Muscle Up Program/muscleuppath.webp",
+        "intro_text": [
+            "The <strong class=\"program-name\">DevCalisthenics Muscle-Up Program</strong> is designed to help you achieve your first muscle-up by focusing on three key goals: <span class=\"span-words\">pull strength, dip strength, and transition</span>",
+            "Through a series of carefully structured exercises, you will develop essential <span class=\"span-words\">pulling strength</span> that is essential in achieving your first muscle-up, improve <span class=\"span-words\">dip strength</span> with exercises such as dips, to help build a solid<span class=\"span-words\"> transition from pull to dip.</span> This program aims to equip calisthenics athletes with the skills and confidence needed to achieve their first muscle-up, setting the stage for long-term fitness and functional strength.",
+            "You can download the complete <span class=\"span-words\">muscle-up program</span> in PDF format by clicking the button below or by scrolling to the bottom of the page to have it sent to your email."
+        ],
+        "goals": [
+            {"image": "../../static/assets/Programs/Muscle Up Program/pullstrength.webp", "text": "Building Foundational Pulling Strength"},
+            {"image": "../../static/assets/Programs/Muscle Up Program/muscleup.webp", "text": "Developing Baseline Dipping Strength"},
+            {"image": "../../static/assets/Programs/Muscle Up Program/transition.webp", "text": "Improving Transition w/ Control & Coordination"}
+        ],
+        "program_details": [
+            "Comprehensive Warm-Up & Mobility Routine",
+            "Foundational Pulling Exercises & Progressions",
+            "Dip Strength Training Routine",
+            "Transition Control & Coordination Routine",
+            "Video Tutorials for Each Exercise",
+            "Progress Tracking"
+        ],
+        "equipment_needed": [
+            "Open Space (Outdoors or Indoors)",
+            "Exercise Mat (Optional)",
+            "Pull-Up Bar",
+            "Resistance Bands",
+            "Gymnastics Rings"
+        ],
+        "carousel_images": [
+            "../../static/assets/Programs/Base/Programs/planche.webp",
+            "../../static/assets/Programs/Base/Programs/invertedgymnast.webp",
+            "../../static/assets/Programs/Base/Programs/reverseplank.webp",
+            "../../static/assets/Programs/Base/Programs/ringsview.webp",
+            "../../static/assets/Programs/Base/Programs/dipsoutdoor.webp",
+            "../../static/assets/Programs/Base/Programs/equipment_img.webp",
+            "../../static/assets/Programs/Base/Programs/pullupgroup.webp",
+            "../../static/assets/Programs/Base/Programs/streetworkout.webp"
+        ]
+    },
 }
