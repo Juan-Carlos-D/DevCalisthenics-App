532b57204d4ad31af28bae5fdebb6b4760f1239e
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-08-20 17:16:00 -0700

    faq

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-# from faq import faq_data 
+from faq import faq_data 
 from macronutrients import macronutrients_data
 from macroscalc import calculate_macros
 # from exercises import exercises
@@ -32,6 +32,11 @@ def macrocalculator():
         return render_template('Nutrition/macroscalc.html', macros=macros, weight=weight, height=height, age=age, gender=gender, activity_level=activity_level, goal=goal, weight_unit=weight_unit, height_unit=height_unit)
 
     return render_template('Nutrition/macroscalc.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('FAQ/FAQ.html', faq_data=faq_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ faq.py
@@ -0,0 +1,24 @@
+# FAQ Q&A
+
+faq_data = [
+    {
+        "question": "What is calisthenics?",
+        "answer": "Calisthenics is a form of strength training that utilizes an individual's body weight as resistance to perform multi-joint compound movements with little or no equipment."
+    },
+    {
+        "question": "How do I know which program is right for me?",
+        "answer": "First, decide what goals you want to accomplish. Then, pick a program that cloely aligns with that goal. If you are a beginner, it is recommended to start with the beginner program to build a strong foundation."
+    },
+    {
+        "question": "What equipment do I need? Can I workout at home?",
+        "answer": "You can absolutely do these workouts in the comfort of your home. Some programs may require equipment, but the program description will have the required equipments before any purchases are made."
+    },
+    {
+        "question": "How will I receive my program?",
+        "answer": "We will email you a PDF that includes everything you need to be successful in your program."
+    },
+    {
+        "question": "What do I do if I lose access to my program?",
+        "answer": "You can talk to our AI agent, 'Nova', or email us and we'll gladly help you out :)"
+    }
+]
