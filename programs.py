e7d9d53487aeb26c956cf67c2beec025c8e814ee
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-08-21 10:59:07 -0700

    programs

--- programs.py
+++ programs.py
@@ -41,5 +41,45 @@ programs_data = {
             "../../static/assets/Programs/Base/Programs/streetworkout.webp"
         ]
     },
+    "handstand_program": {
+        "title": "Welcome to the Handstand Program!",
+        "intro_image": "../../static/assets/Programs/Handstand Program/handstand.webp",
+        "intro_text": [
+            "The <strong class=\"program-name\">DevCalisthenics Handstand Program</strong> is designed to help you achieve your first handstand by focusing on three key goals: <span class=\"span-words\">core strength, scapula control, and shoulder strength.</span>",
+            "Through a series of carefully structured exercises, you will develop essential <span class=\"span-words\">scapula control</span> that supports all handstand movement, improve <span class=\"span-words\">core strength</span> with exercises such as hollow body, and build solid <span class=\"span-words\">shoulder strength.</span> This program aims to equip beginners with the skills and confidence needed to achieve their first handstand, setting the stage for long-term fitness and functional strength.",
+            "You can download the complete <span class=\"span-words\">handstand program</span> in PDF format by clicking the button below or by scrolling to the bottom of the page to have it sent to your email."
+        ],
+        "goals": [
+            {"image": "../../static/assets/Programs/Handstand Program/hollowbody.webp", "text": "Building Foundational Core Strength"},
+            {"image": "../../static/assets/Programs/Handstand Program/shoulderstrength.webp", "text": "Developing Baseline Shoulder Strength"},
+            {"image": "../../static/assets/Programs/Handstand Program/scapulacontrol.webp", "text": "Improving Scapula Control & Coordination"}
+        ],
+        "program_details": [
+            "Comprehensive Warm-Up & Mobility Routine",
+            "Foundational Core Strength Exercises",
+            "Shoulder Strength Training Routine",
+            "Scapula Control & Coordination Routine",
+            "Video Tutorials for Each Exercise",
+            "Progress Tracking"
+        ],
+        "equipment_needed": [
+            "Open Space (Outdoors or Indoors)",
+            "Exercise Mat (Optional)",
+            "Pull-Up Bar",
+            "Resistance Bands (Optional)",
+            "Dumbbells",
+            "Gymnastics Rings"
+        ],
+        "carousel_images": [
+            "../../static/assets/Programs/Base/Programs/planche.webp",
+            "../../static/assets/Programs/Base/Programs/invertedgymnast.webp",
+            "../../static/assets/Programs/Base/Programs/reverseplank.webp",
+            "../../static/assets/Programs/Base/Programs/ringsview.webp",
+            "../../static/assets/Programs/Base/Programs/dipsoutdoor.webp",
+            "../../static/assets/Programs/Base/Programs/equipment_img.webp",
+            "../../static/assets/Programs/Base/Programs/pullupgroup.webp",
+            "../../static/assets/Programs/Base/Programs/streetworkout.webp"
+        ]
+    },
     # Add other programs here
 }
